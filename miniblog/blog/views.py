0d9e824af2fea7f7a34c5ab1b94293cdf89f38ee
Tulsishah <tulsishatms@gmail.com>
2021-03-11 22:41:47 +0530

    done
    done

--- miniblog/blog/views.py
+++ miniblog/blog/views.py
@@ -34,8 +34,6 @@ def user_login(request):
                login(request,user)
                messages.success(request,'LOGIN SUCCESFULLY !!')
                return HttpResponseRedirect('/dashboard/')
-            else :
-               messages.error(request,'LOGIN SUCCESFULLY !!')
       else :
          form = Login()
       return render(request,'blog/login.html',{'form':form})
@@ -65,8 +63,13 @@ def signup(request):
 
 def dashboard(request):
       posts = Post.objects.all()   
-      return render(request,'blog/dashboard.html',{'posts':posts})
+      if request.user.is_authenticated:
+         Uname=request.user.username
+         return render(request,'blog/dashboard.html',{'posts':posts,'Uname':Uname})
+      else :
+          return render(request,'blog/dashboard.html',{'posts':posts})
 
+          
 def Addpost(request):
    if request.user.is_authenticated:
       if request.method == "POST":
@@ -75,7 +78,8 @@ def Addpost(request):
             title=form.cleaned_data['title']
             desc=form.cleaned_data['desc']
             university=form.cleaned_data['university']
-            pst=Post(title=title,desc=desc,university=university)
+            uname=form.cleaned_data['uname']
+            pst=Post(title=title,desc=desc,university=university,uname=uname)
             pst.save()
             form = Postform()
             messages.success(request,'Add successfully !!')
